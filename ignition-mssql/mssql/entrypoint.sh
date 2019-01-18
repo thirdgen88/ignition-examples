b257727101313799f47bcb3bcf8aefefb293fc3b
Kevin Collins <thirdgen88@gmail.com>
2019-01-17 21:21:55 -0600

    Updates to mssql example

--- ignition-mssql/mssql/entrypoint.sh
+++ ignition-mssql/mssql/entrypoint.sh
@@ -2,15 +2,56 @@
 set -eo pipefail
 shopt -s nullglob
 
-MSSQL_BASE=/var/opt/mssql
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+# usage: process_init_file FILENAME SQLCMD...
+#    ie: process_init_file foo.sh sqlcmd -S localhost -u sa
+# (process a single initializer file, based on its extension. we define this
+# function here, so that initializer scripts (*.sh) can use the same logic,
+# potentially recursively, or override the logic used in subsequent calls)
+process_init_file() {
+	local f="$1"; shift
+	local sqlcmd=( "$@" )
+
+	case "$f" in
+		*.sh)     echo "$0: running $f"; . "$f" ;;
+		*.sql)    echo "$0: running $f"; "${sqlcmd[@]}" -i "$f"; echo ;;
+        *.bak)    echo "$0: restoring $f"; "${sqlcmd[@]}" -Q "RESTORE DATABASE [$(basename ${f/.bak/})] FROM DISK='$f'"; echo ;;
+		*)        echo "$0: ignoring $f" ;;
+	esac
+	echo
+}
+
+MSSQL_BASE=${MSSQL_BASE:-/var/opt/mssql}
 MSSQL_PROVISIONING_FILE=${MSSQL_BASE}/dbsetup.sql
+file_env 'SA_PASSWORD' 'ch@nge_m3'
 export SQLCMDPASSWORD=$SA_PASSWORD
 
 # Collect set default values for supported environment variables
-MSSQL_DATABASE=${MSSQL_DATABASE:-ignition}
-MSSQL_USER=${MSSQL_USER:-ignition}
-MSSQL_PASSWORD=${MSSQL_PASSWORD:-ignition}
-MSSQL_PID=${MSSQL_PID:-Developer}
+file_env 'MSSQL_DATABASE' 'test'
+file_env 'MSSQL_USER' 'test'
+file_env 'MSSQL_PASSWORD' 'test'
+file_env 'MSSQL_PID' 'developer'
 
 # Check for Init Complete
 if [ ! -f "${MSSQL_BASE}/.docker-init-complete" ]; then
@@ -57,6 +98,13 @@ EOF
         echo >&2 "Failed to provision database."
         exit 1
     fi
+    sqlcmd=( sqlcmd -S localhost -U sa -l 3 -t 3 -V 16 )
+
+    echo
+    ls /docker-entrypoint-initdb.d/ > /dev/null
+    for f in /docker-entrypoint-initdb.d/*; do
+        process_init_file "$f" "${sqlcmd[@]}"
+    done
 
     # Attach and wait for exit
     wait "$pid"
